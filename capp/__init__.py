000c3d7f6ad7d6cc9e1602314789b4477668fcb8
JoakimBreivik <s242920@nhhs.no>
2025-04-11 23:51:57 +0200

    Connected AWS DB

--- capp/__init__.py
+++ capp/__init__.py
@@ -7,11 +7,12 @@ import os
 
 application = Flask(__name__)
 
-# application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
-application.config['SECRET_KEY'] = 'S_key'
 
-application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
-application.config['SQLALCHEMY_BINDS'] ={'transport': 'sqlite:///transport.db'}
+application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
+DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
+application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
+application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
+
 
 db = SQLAlchemy(application)
 bcrypt = Bcrypt(application)
