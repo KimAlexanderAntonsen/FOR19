1af4a2fdbb559e9e716c4f6638c7f510f8c64f6c
Mr3015 <s242773@nhhs.no>
2025-04-12 15:03:04 +0200

    Final new entry form, but misses real submit logic

--- capp/carbon_app/routes.py
+++ capp/carbon_app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, Blueprint
 from flask_login import login_required
-
+from capp.carbon_app.forms import BusForm, CarForm, PlaneForm, FerryForm, MotorcycleForm, BicycleForm, WalkForm, TrainForm
 carbon_app = Blueprint("carbon_app", __name__)
 
 @carbon_app.route("/carbon_app")
@@ -11,8 +11,30 @@ def carbon_app_home():
 @carbon_app.route('/carbon_app/new_entry_bus', methods=['GET','POST'])
 @login_required
 def new_entry_bus():
-    return render_template('carbon_app/new_entry_bus.html', title='new entry bus')
+    form = BusForm()
+    if form.validate_on_submit():
+        kms = form.kms.data
+        fuel = form.fuel_type.data
+        transport = 'Bus'
+        # kms = request.form['kms']
+        # fuel = request.form['fuel_type']
 
+        co2 = float(kms) * efco2[transport][fuel]
+        ch4 = float(kms) * efch4[transport][fuel]
+        total = co2+ch4
+
+        co2 = float("{:.2f}".format(co2))
+        ch4 = float("{:.2f}".format(ch4))
+        total = float("{:.2f}".format(total))
+
+        emissions = Transport(kms=kms, transport=transport,
+            fuel=fuel, co2=co2, ch4=ch4, total=total, author=current_user)
+        db.session.add(emissions)
+        db.session.commit()
+        return redirect(url_for('carbon_app.your_data'))
+    
+    return render_template('carbon_app/new_entry_bus.html', 
+        title='new entry bus', form=form)
 
 @carbon_app.route('/carbon_app/new_entry_car', methods=['GET','POST'])
 @login_required
